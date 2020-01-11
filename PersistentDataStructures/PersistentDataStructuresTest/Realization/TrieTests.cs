aab82d765b0ca8d14ca4b5b43d860fc9d219c67f
AnDeRR <anderr225@gmail.com>
2020-01-12 05:25:57 +0700

    feat: add tests for updating and adding trie operations

--- PersistentDataStructures/PersistentDataStructuresTest/Realization/TrieTests.cs
+++ PersistentDataStructures/PersistentDataStructuresTest/Realization/TrieTests.cs
@@ -47,14 +47,13 @@ namespace TestProject1.Realization
         [Fact]
         public void ShouldFindElementOutOfMillionElements()
         {
-            int[] insertedElements = new int[10_000_001];
+            int[] insertedElements = new int[1_000_000];
             insertedElements[50] = 50;
             insertedElements[100] = 100;
             insertedElements[1000] = 1000;
             insertedElements[4444] = 4444;
             insertedElements[32767] = 4444;
             insertedElements[999999] = 999999;
-            insertedElements[10_000_000] = 999999;
 
             var trie = new PersistentTrie<int>(insertedElements);
 
@@ -65,7 +64,63 @@ namespace TestProject1.Realization
             Assert.Equal(insertedElements[32767], trie.Find(32767));
             Assert.Equal(insertedElements[99999], trie.Find(99999));
             Assert.Equal(insertedElements[999999], trie.Find(999999));
-            Assert.Equal(insertedElements[10_000_000], trie.Find(999999));
+        }
+        
+        [Fact]
+        public void Should_UpdateElementWithoutTouchingLastVersion()
+        {
+            int[] insertedElements = new int[1_000_000];
+            insertedElements[50] = 50;
+            insertedElements[100] = 100;
+            insertedElements[999999] = 999999;
+
+            var trie = new PersistentTrie<int>(insertedElements);
+            var updatedTrie = trie.Update(999999, -51);
+
+            Assert.Equal(insertedElements[50], trie.Find(50));
+            Assert.Equal(insertedElements[50], updatedTrie.Find(50));
+            Assert.Equal(insertedElements[100], trie.Find(100));
+            Assert.Equal(insertedElements[100], updatedTrie.Find(100));
+            Assert.Equal(insertedElements[999999], trie.Find(999999));
+            Assert.Equal(-51, updatedTrie.Find(999999));
+        }
+
+        [Fact]
+        public void Should_UpdateSeveralTimes()
+        {
+            var firstTrie = new PersistentTrie<int>(0, 1);
+            var secondTrie = firstTrie.Update(0, 2);
+            var thirdTrie = secondTrie.Update(0, 3);
+
+            Assert.Equal(0, firstTrie.Find(0));
+            Assert.Equal(2, secondTrie.Find(0));
+            Assert.Equal(3, thirdTrie.Find(0));
+            
+            Assert.Equal(1, firstTrie.Find(1));
+            Assert.Equal(1, secondTrie.Find(1));
+            Assert.Equal(1, thirdTrie.Find(1));
+        }
+        
+        [Fact]
+        public void Should_AddFirstElement()
+        {
+            var trie = new PersistentTrie<int>();
+            var secondTrie = trie.Add(22);
+
+            Assert.Equal(0, trie.Length);
+            Assert.Equal(1, secondTrie.Length);
+            Assert.Equal(22, secondTrie.Find(0));
+        }
+        
+        [Fact]
+        public void Should_IncreaseDepthWithAdd()
+        {
+            var trie = new PersistentTrie<int>(new int[32]);
+            var secondTrie = trie.Add(22);
+
+            Assert.Equal(1, trie.Depth);
+            Assert.Equal(2, secondTrie.Depth);
+            Assert.Equal(22, secondTrie.Find(32));
         }
     }
 }
